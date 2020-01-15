7931df29292a8c25101a164e1eecc41ab862d02f
Oleg Kolpakov <00legas00@gmail.com>
2020-01-15 18:46:40 +0300

    1. Create modal window with video

--- js/script.js
+++ js/script.js
@@ -8,4 +8,13 @@ $(function ($) {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $('.open-video').magnificPopup({
+        disableOn: 700,
+        type:'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
 });

8a1fffb24d95aa998d96cf1513f9b3b8982681c9
Oleg Kolpakov <00legas00@gmail.com>
2020-01-26 20:16:20 +0300

    Create info-block

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
 $(function ($) {
     "use strict";
 
-    $('.home-slider').slick({
-        vertical: true,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+    if ($('.home-slider').length) {
+        $('.home-slider').slick({
+            vertical: true,
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 2000
+        });
+    }
 
-    $('.open-video').magnificPopup({
-        disableOn: 700,
-        type:'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
+    //carousel
+    if ($('.shop-carousel').length) {
+        $('.shop-carousel').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            appendArrows: $('.shop-slider-navigation'),
+            prevArrow: $('.left'),
+            nextArrow: $('.right')
+        });
+    }
+
+    if ($('.open-video').length) {
+        $('.open-video').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    }
 });

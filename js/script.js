6e8c69046c70ffa5f43da54ebf687c3cc7c96b6d
Oleg Kolpakov <00legas00@gmail.com>
2020-02-08 21:31:43 +0300

    Create widgets and begin create top-footer section.

--- js/script.js
+++ js/script.js
@@ -33,4 +33,35 @@ $(function ($) {
             fixedContentPos: false
         });
     }
+
+    if ($('.testy-gallery').length) {
+        $('.testy-gallery').magnificPopup({
+            type: 'image',
+            classOnContentClick: true,
+            mainClass: 'mfp-img-mobile',
+            image: {
+                verticalFit: true
+            }
+        });
+    }
+
+/*
+        $('.testimonial-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                }
+            }
+        });
+ */
 });
